49de309615ae875b97c92aa9587e44555db822d9
youxuehu <youxuehu@hotmail.com>
2019-07-03 23:27:07 +0800

    tijiao

---
+++ src/main/java/com/fileupload/Test.java
@@ -0,0 +1,11 @@
+package com.fileupload;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<Integer,String> map = new HashMap<>();
+        map.put(1,"jack");
+    }
+}
